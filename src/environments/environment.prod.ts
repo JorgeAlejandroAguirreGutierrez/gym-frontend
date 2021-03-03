e8461df45921982e28feaea36ce6a71e44483d19
alejandro <JAGUIR4@bancodebogota.com.co>
2021-03-03 08:52:14 -0500

    mod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   host:'https://gym-backend1.herokuapp.com/',
-  prefijo_url_imagenes: 'https://gym-backend1.herokuapp.com/storage/'
+  prefijo_url_imagenes: 'https://gym-backend1.herokuapp.com/img/',
+  prefijo_url_ejercicios: 'http://gym-backend1.herokuapp.com/imgejercicio/'
 };
