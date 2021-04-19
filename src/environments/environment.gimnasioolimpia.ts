fede0929405fe1589993da3d8e898b343fbe7805
alejandro <JAGUIR4@bancodebogota.com.co>
2021-04-19 15:42:40 -0500

    mod

--- src/environments/environment.gimnasioolimpia.ts
+++ src/environments/environment.gimnasioolimpia.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   host:'https://gimnasioolimpia-backend.herokuapp.com/',
   prefijo_url_imagenes: 'https://gimnasioolimpia-backend.herokuapp.com/img/',
-  prefijo_url_ejercicios: 'http://gimnasioolimpia-backend.herokuapp.com/imgejercicio/',
+  prefijo_url_ejercicios: 'https://gimnasioolimpia-backend.herokuapp.com/imgejercicio/',
   gimnasio: "GIMNASIO OLIMPIA",
   ubicacion: "SALAMINA CALDAS"
 };
